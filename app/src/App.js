bc50d47647b35ade4638147b054aeada05752f90
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2021-02-02 18:52:09 -0500

    feat(deploy): switch from Router to BrowserRouter

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Index from './Pages/Index';
+import { Route, Switch, BrowserRouter} from 'react-router-dom';
+import React, { Component } from 'react';
 
 
 function App() {
   return (
     <div className="App">
-      <Index />
+      <BrowserRouter>
+        <Switch>
+          <Route path = "/" component = {Index}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
